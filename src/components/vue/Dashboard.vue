23e5ba43e77a9c7ad2b1c036d85b575c5f515129
Humberto Jr <91671380+HumbertoJrGH@users.noreply.github.com>
2024-03-14 17:00:37 -0300

    ajuste pequeno

---
+++ src/components/vue/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="dashboard">
+		<h1>Dashboard</h1>
+		<p>This is a simple Vue Dashboard built with Astro.js!</p>
+		<Project />
+	</div>
+</template>
+
+<script setup>
+import Project from "./Project.vue"
+</script>
+
+<style>
+.dashboard {
+	margin: 0 auto;
+	max-width: 1024px;
+}
+</style>
---
+++ src/components/vue/Project.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="project">
+		<h2>{{ project.title }}</h2>
+		<p>{{ project.description }}</p>
+		<ul>
+			<li v-for="tech in project.technologies" :key="tech">{{ tech }}</li>
+		</ul>
+	</div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+const project = ref({
+	title: "This is a title.",
+	description: "This is a description.",
+	technologies: ["Vue.js", "Astro.js", "typescript", "JavaScript", "HTML", "CSS"]
+})
+</script>
+
+<style scoped>
+.project {
+	background-color: black;
+	width: 500px;
+	color: #ffffff66;
+	padding: 1rem;
+	border-radius: 1rem;
+	margin-bottom: 20px;
+}
+</style>
